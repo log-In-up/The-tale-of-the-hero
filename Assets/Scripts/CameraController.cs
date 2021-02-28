624f6e4ee95b1c9c231dc0e1b797690115f71be1
Ruslan Vyasel'kin <rus123rus98@gmail.com>
2021-02-28 19:04:56 +0300

    Added camera controller.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using static UnityEngine.Vector3;
+
+sealed class CameraController : MonoBehaviour
+{
+    #region Parameters
+    [SerializeField] private float cameraSpeed = 2.0f;
+
+    private Camera playerCamera = null;
+
+    public static CameraController Instance { get; private set; }
+    #endregion
+
+    #region MonoBehaviour API
+    private void Awake()
+    {
+        #region Singleton
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        #endregion
+
+        playerCamera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        MoveCamera();
+    }
+    #endregion
+
+    #region Custom methods
+    private void MoveCamera()
+    {
+        transform.position = Lerp(
+            transform.position,
+            PlayerController.Instance.transform.position,
+            cameraSpeed * Time.deltaTime);
+    }
+    #endregion
+}
